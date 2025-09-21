c97374da575a07e4d5b1f8a93ebbdcc14d50510b
Bruno Martins <bgcngm@gmail.com>
2025-09-21 21:45:05 +0100

    xueying: overlay: Drop UDFPS related configs
    This device has side-mounted fingerprint sensor.
    
    Change-Id: I53c3cd8d2775daec361b9f48a1a43809949af428

--- device.mk
+++ device.mk
@@ -34,7 +34,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
-    OPlusSettingsResTarget \
     OPlusSystemUIResTarget
 
 # Regional properties
